bc21c37d18f40488fb47ce69cea01fffdd7b9ad5
mfadhili <mfadhili@github.com>
2022-10-22 12:57:05 +0300

    testing login api

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 
 const apiRoutes = require('./src/routes/mainRouter');
 
 const app = express();
 const port = 8000;
 
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
