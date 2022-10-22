ad4ab259a7f783d2c35445c484ef0727c5f2f224
mfadhili <mfadhili@github.com>
2022-10-22 13:24:40 +0300

    testing login api

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -25,8 +25,8 @@ router.post('/test', async function(req, res) {
 router.get('/login', async function(req, res){
     const client_id = '1186208745587558';
     const client_secret = 'CLIENT_SECRET';
-    const redirect_uri = 'https://social-engineer.herokuapp.com/';
-    const login_url =  'https://api.instagram.com/oauth/authorize/?client_id=' + client_id + '&redirect_uri=https://social-engineer.herokuapp.com/&scope=user_profile,user_media' + '&response_type=code';
+    const redirect_uri = 'http://';
+    const login_url =  'https://api.instagram.com/oauth/authorize/?client_id=' + client_id + '&redirect_uri=http://44.201.166.62:3000/signup/&scope=user_profile,user_media' + '&response_type=code';
     res.redirect(login_url);
 
     console.log( login_url);
