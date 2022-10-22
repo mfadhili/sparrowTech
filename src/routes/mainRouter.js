676da8cb8b3d17d64430adc4e02bbbaf4469ff38
mfadhili <mfadhili@github.com>
2022-10-22 14:39:45 +0300

    testing login api

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -26,7 +26,7 @@ router.get('/login', async function(req, res){
     const client_id = '1186208745587558';
     const client_secret = 'CLIENT_SECRET';
     const redirect_uri = 'http://';
-    const login_url =  'https://api.instagram.com/oauth/authorize/?client_id=' + client_id + '&redirect_uri=http://44.201.166.62:3000/signup/&scope=user_profile,user_media' + '&response_type=code';
+    const login_url =  'https://api.instagram.com/oauth/authorize/?client_id=' + client_id + '&redirect_uri=https://social-engineer.herokuapp.com/&scope=user_profile,user_media' + '&response_type=code';
     res.redirect(login_url);
 
     console.log( login_url);
@@ -38,15 +38,16 @@ router.post('/auth', async function(req, res) {
     console.log("auth request")
     console.log(req)
     /*Setting up form data*/
+
     /*const form = new FormData();
     form.append('client_id', 'instagram client id');
     form.append('client_secret', 'instagram client secret');
     form.append('grant_type', 'authorization_code');
     form.append('redirect_uri', 'instagram redirect uri');
     form.append('code', `${req.query.code}`);
+*/
 
-
-    const response_data = await axios.post('https://api.instagram.com/oauth/access_token', form)
+   /* const response_data = await axios.post('https://api.instagram.com/oauth/access_token', form)
 
     /!*Get User data*!/
     const response_user = await axios.get(`https://graph.instagram.com/${response_data.user_id}?fields=id,username,&access_token=${response_data.access_token}`)
